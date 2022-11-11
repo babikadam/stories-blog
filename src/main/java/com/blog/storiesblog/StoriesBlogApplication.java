288904e1d25510da1f89534fd0fc446f89f37278
babikadam <adam.babik@brightstraining.com>
2022-11-11 10:36:24 +0100

    initial commit

---
+++ src/main/java/com/blog/storiesblog/StoriesBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.storiesblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoriesBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoriesBlogApplication.class, args);
+	}
+
+}

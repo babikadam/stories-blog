d24671364fe705b99c23fb2a2befe0b9bd7ca92e
babikadam <adam.babik@brightstraining.com>
2022-11-15 18:04:53 +0100

    something

--- src/main/java/com/blog/storiesblog/security/WebSecurityConfig.java
+++ src/main/java/com/blog/storiesblog/security/WebSecurityConfig.java
@@ -2,15 +2,21 @@ package com.blog.storiesblog.security;
 
 
 import com.blog.storiesblog.service.CustomUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -25,9 +31,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http    .authorizeRequests()
+        http
+                .authorizeRequests()
                 .antMatchers(WHITELIST).permitAll()
-                .antMatchers(HttpMethod.GET, "/posts/*").permitAll()
+//                .antMatchers(HttpMethod.GET, "/posts/savePost/**", "/posts/editPost/**").hasRole("USER")
                 .anyRequest().authenticated();
 
         http    .formLogin()
@@ -50,6 +57,6 @@ public class WebSecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    
+
+
 }

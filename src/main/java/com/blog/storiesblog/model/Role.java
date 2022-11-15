5d5ce2f43636089768e11d824a75f4cbcb301387
babikadam <adam.babik@brightstraining.com>
2022-11-15 17:48:29 +0100

    role commit

--- src/main/java/com/blog/storiesblog/model/Role.java
+++ src/main/java/com/blog/storiesblog/model/Role.java
@@ -1,28 +1,39 @@
 package com.blog.storiesblog.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.io.Serializable;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Data
 @Entity
 @Table (name = "roles")
-public class Role implements Serializable {
+public class Role {
 
     @Id
-    @Column
-    private String name;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role", unique = true, nullable = false)
+    private String role;
 
     public Role() {
     }
 
-    public String getName() {
-        return name;
+    public Long getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
 
